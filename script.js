bc74c9af16fa5b35c2f28e3f62e22ef2e201a03e
Adrian Jeż <ajez@i2asolutions.com>
2017-06-13 20:32:12 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,34 +1,91 @@
-//Config properties
-var BASE_URL = "http://gameofthrones.wikia.com/";
+//** Additional Module - filters provider 
+var additionalModule = angular.module("filtersProvider", [])
+additionalModule.filter('appendLength', function() {
+    return function(x) {
+    	var result = x + " o dlugosci: " + x.length;
+    	return result;
+    };
+});
+additionalModule.filter('makeMirror', function() {
+	return function(x) {
+		var result = x + '_';
 
-var app = angular.module("mainModule", []);
-
-app.controller('ArticlesController', function ($scope, $http) {
-
-	$scope.rates = {};
-    $http.get(BASE_URL + 'api/v1/Articles/Popular?expand=1&limit=10')
-		.then(function(res) {
-			$scope.articles = data.results;
-		});
-            
-
+		for(var i = x.length - 1; i >= 0; i--){
+			result += x[i];
+		}
+		return result;
+	}
 });
 
 
-app.controller("HttpController", [ '$scope', '$resource',
-		function($scope, $resource) {
-			//
-			// GET Action Method
-			//
-			var User = $resource('/user/:userId', {userId:'@userId'});
-			User.get( {userId:25}, function(user){
-				$scope.profile = user;
-			})
-			//
-			// Query Action Method
-			//
-			var UserProfiles = $resource(BASE_URL + 'api/v1/Articles/Popular?expand=1&limit=10');
-			UserProfiles.query(function(profiles){
-				$scope.profiles = profiles;					
+var app = angular.module("mainModule", ["filtersProvider"]);
+
+//** Factories **/
+app.factory("services", ['$http', function($http) {
+	var BASE_URL = 'https://quiet-castle-94433.herokuapp.com/'
+	var obj = {};
+	obj.getDogs = function(){
+    	return $http.get(BASE_URL + 'dogs');
+	}
+
+	obj.insertDog = function (dog) {
+  		return $http.post(BASE_URL + 'dogs/add', dog).then(function (results) {
+      		return results;
+  		})
+	};
+	return obj;
+}]);
+
+/** Controllers **/
+app.controller('DogsCreationController', function ($scope, services) {
+	$scope.addDog = function(){
+		var dog = {};
+		dog.rasa = $scope.creator_Rasa;
+		dog.sredniWiekZycia = $scope.creator_SredniWiekZycia;
+		dog.maksymalnaPredkoscBiegu = $scope.creator_MaksymalnaPredkoscBiegu;
+		dog.zdjecie = $scope.creator_Zdjecie;
+		dog.opis = $scope.creator_Opis;
+		dog.cena = $scope.creator_Cena;
+
+		$scope.creator_Rasa = "";
+		$scope.creator_SredniWiekZycia = "";
+		$scope.creator_MaksymalnaPredkoscBiegu = "";
+		$scope.creator_Zdjecie = "";
+		$scope.creator_Cena = "";
+		$scope.creator_Opis = "";
+
+		services.insertDog(dog).then(function(data){
+			$scope.dogs.push(data.data);
+		});
+	}
+});
+
+app.controller("DateTodayController", function($scope) {
+	$scope.dzisiejszaData = new Date();
+});
+
+app.controller("FiltersProviderTester", function($scope){
+	$scope.testData = ["Adrian", "Pawel", "Robert", "Dominik", "Jan", "Andrzej", "Krzysztof"];
+});
+
+/** Directives **/
+app.directive("dogsList", ['services', function(services) {
+	return {
+    	controller: function ($scope) {
+     		services.getDogs().then(function(data){
+				$scope.dogs = data.data;
 			});
-		} ]);
+    	},
+		templateUrl : "szablony/dogs_list.html"
+  	};
+}]);
+
+app.directive("dogItem", function(){
+	return {
+		scope: {
+        	ngModel: '='
+    	},
+		templateUrl : "szablony/dog_item.html"
+	};
+});
+
